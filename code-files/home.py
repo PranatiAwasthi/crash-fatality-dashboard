9f123c3c871c96118ad687e25cf8ac7efbcf94b5
PranatiAwasthi <101756257+PranatiAwasthi@users.noreply.github.com>
2022-04-01 17:43:49 -0700

    Update home.py

--- code-files/home.py
+++ code-files/home.py
@@ -3,5 +3,6 @@ import settings
 def app():
     st.title("Project- Data Management and Visualization")
     st.header("Welcome to the Crash/Fatalities Dashboard!")
+    st.subheader("Developed by: Yinsheng and Pranati")
     st.subheader("sneak peek of data")
     st.write(settings.get_data().astype(str).head())
